5ecfe826ef16c890f2a0af69bf30dc7f2b223d50
Joe Turner <joe@oampo.co.uk>
2017-07-24 14:03:01 +0100

    Reorder

--- src/validators.js
+++ src/validators.js
@@ -1,8 +1,8 @@
-export const required = value => value ? undefined : 'Required';
+export const required = value => (value ? undefined : 'Required');
 export const nonEmpty = value =>
     value.trim() !== '' ? undefined : 'Cannot be empty';
-export const matches = field => (value, allValues) =>
-    field in allValues && value.trim() === allValues[field].trim() ? undefined : 'Does not match';
+export const isTrimmed = value =>
+    value.trim() === value ? undefined : 'Cannot start or end with whitespace';
 export const length = length => value => {
     if (length.min && value.length < length.min) {
         return `Must be at least ${length.min} characters long`;
@@ -10,6 +10,8 @@ export const length = length => value => {
     if (length.max && value.length > length.max) {
         return `Must be at most ${length.max} characters long`;
     }
-}
-export const isTrimmed = value =>
-    value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+};
+export const matches = field => (value, allValues) =>
+    field in allValues && value.trim() === allValues[field].trim()
+        ? undefined
+        : 'Does not match';
