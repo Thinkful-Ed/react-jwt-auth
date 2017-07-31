7e207213b540ee241acdd5ad1b2bc04f34fb40fd
Joe Turner <joe@oampo.co.uk>
2017-07-31 10:49:59 +0100

    Don't disable invalid login form.

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -41,7 +41,7 @@ export class LoginForm extends React.Component {
                     id="password"
                     validate={[required, nonEmpty]}
                 />
-                <button disabled={this.props.invalid || this.props.submitting}>
+                <button disabled={this.props.pristine || this.props.submitting}>
                     Log in
                 </button>
             </form>
